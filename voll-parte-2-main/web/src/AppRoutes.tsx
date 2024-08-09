b6e93b9f29f487da76881beab396dcac8f79a85b
gleidson~ <gbtavares@stefanini.com>
2024-08-09 10:58:22 -0300

    first commit

---
+++ voll-parte-2-main/web/src/AppRoutes.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import PaginaBase from "./pages/PaginaBase";
+import PaginaInicial from "./pages/PaginaInicial";
+
+function AppRoutes() {
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<PaginaBase />}>
+            <Route index element={<PaginaInicial />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes;
